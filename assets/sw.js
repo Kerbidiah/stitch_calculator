a5c1e5a654f9c565c757e22b1241d1cb38782402
Kerbidiah <alexjanninck@gmail.com>
2022-12-16 22:37:15 -0500

    initial stuff

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './stitch_calculator.js',
+  './stitch_calculator_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
